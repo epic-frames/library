1fc1e54463a80cf783267fa4760de32f9d9f8f02
epic-frames <epicframe.email@gmail.com>
2026-09-18 17:47:41 +0000

    webv1.1.0.13

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const version = "webv1.1.0.12"
+const version = "webv1.1.0.13"
 const countdown = true
 const countdownDate = new Date("2026-09-19T12:15:00+03:00").getTime();
 // const countdownExactTime = "16:00:00"
